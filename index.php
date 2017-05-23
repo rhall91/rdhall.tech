41759ecece0cc0c9a4b3a589cd4e7a7e4e0c41d0
Richard Hall <rd.hall91@gmail.com>
2017-05-22 22:18:01 -0400

    adding social media links
    add social media links, font-awesome, some css on the links to grow on
    hover.

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="img/favicon.ico">
-
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <title>Hello, World.</title>
 
     <!-- Bootstrap core CSS -->
@@ -15,7 +15,6 @@
     <!-- Custom styles for this template -->
     <link href="css/style.css" rel="stylesheet">
 </head>
-
 <body>
 
 <div class="site-wrapper">
@@ -29,15 +28,15 @@
                     <h3 class="masthead-brand">Yarr!</h3>
                     <nav class="nav nav-masthead">
                         <a class="nav-link active" href="#">Home</a>
-                        <a class="nav-link" href="#">About</a>
-                        <a class="nav-link" href="#">Contact</a>
+<!--                        <a class="nav-link" href="#">About</a>-->
+<!--                        <a class="nav-link" href="#">Contact</a>-->
                     </nav>
                 </div>
             </div>
 
             <div class="inner cover">
                 <h1 class="cover-heading"><span class="typedElement"></span></h1>
-                <p class="lead">Based on the Space Coast, Florida.</p>
+                <p class="lead">Based on Florida's Space Coast.</p>
                 <p>Check back soon, more to come!</p>
 
             </div>
@@ -45,7 +44,19 @@
             <div class="mastfoot">
                 <div class="inner">
                     <footer>
+                        <div>
+                            <ul class="socialMediaList" style="">
+                                <li><a class="button social" href="https://www.linkedin.com/in/richard-hall-990777127/"><i class="fa fa-linkedin social"></i></a></li>
+                                <li><a href="https://github.com/rhall91" target="_blank"><i class="fa fa-github social" aria-hidden="true"></i></a></li>
+                                <li><a href="mailto:rich@rdhall.tech" target="_blank"><i class="fa fa-envelope social" aria-hidden="true"></i></a></li>
+<!--                                <li><a class="button social" href="https://twitter.com/hallaathrad"><i class="fa fa-fw fa-twitter"></i></a></li>-->
+                            </ul>
+                        </div>
+
+
                         &copy;<?= date('Y');?> Richard Hall
+
+
                     </footer>
                 </div>
             </div>
